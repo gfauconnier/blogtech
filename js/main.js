9fb7e9762a45e6626f96d37c45cc4baab0b9b826
Greg Fauconnier <g.fauconnier@hotmail.fr>
2017-06-19 09:12:39 +0200

    index.html : added js onload - main.js : added code for date and seconds display + categories selection + image hover

---
+++ js/main.js
@@ -0,0 +1,56 @@
+// displays the date/time on load of page
+var dateAj = new Date();
+document.getElementById("dateA").innerHTML = "Le " + dateAj.getDate() + " / " + dateAj.getMonth() + " / " + dateAj.getFullYear() + " - " + dateAj.getHours() + "h" + dateAj.getMinutes();
+
+// check the categories depending on what is selected and send what's to be done to classChange function
+function tabChange(x) {
+  var articles = document.getElementsByTagName('article');
+  var j = 0;
+  var tab = ["hw", "sw", "dev"];
+  for (var i = 0; i < articles.length; i++) {
+    if ((x == 3 || articles[i].classList.contains(tab[x])) && j < 8) {
+      classChange(articles[i], j, 0);
+      j++;
+    } else {
+      classChange(articles[i], j, 1);
+    }
+  }
+}
+
+// adds or remove classes to display articles depending on selected category and what tabChange() sent
+function classChange(art, j, n) {
+  if (j <= 1 && n == 0) {
+    art.classList.remove("smallArticle");
+    art.classList.remove("hideArticle");
+    art.style.order = j-4;
+  } else if (j > 1 && n == 0) {
+    art.classList.add("smallArticle");
+    art.classList.remove("hideArticle");
+    art.style.order = 0;
+  } else {
+    art.classList.add("hideArticle");
+  }
+}
+
+// Displays the alt attribute of the image inside a p element thats displayed over the image
+function imgHover(th, z) {
+  var art = "art" + th.id[3];
+  var img = "img" + th.id[3];
+  if (z == 1) {
+    var alt = document.getElementById(img).alt;
+    document.getElementById(art).innerHTML = alt;
+  } else {
+    document.getElementById(art).innerHTML = "";
+  }
+}
+
+// Displays how many seconds the user is on the page
+function tempsP() {
+  var t = setTimeout("tempsP()", 1000);
+  document.getElementById("temps").innerHTML = "Temps passé : " + t + " secondes";
+}
+
+// Confirm validation of articles' comments (atm not doing anything)
+function confirmA() {
+  confirm("Voulez-vous confirmer la saisie du commentaire ?");
+}
