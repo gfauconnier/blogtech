cc687e941ee328c6cafc8ae3a30d7d0283f33438
Greg Fauconnier <g.fauconnier@hotmail.fr>
2017-06-21 15:18:08 +0200

    worked on wordpress theme, still pretty oblivious on how it works

---
+++ blog/wp-content/themes/blogtech-theme/sidebar.php
@@ -0,0 +1,14 @@
+
+      <!-- ################## Aside only for desktop ######################### -->
+      <aside class="asideDesktop hidden-md-down col-lg-3 card">
+        <a href="#"><img src="img/pub/pub1.jpeg" alt="PUB"></a>
+        <nav class="nav flex-column align-items-baseline">
+          <a href="#"><img src="img/uparrow.svg" height="50px" alt="Home"></a>
+          <p>Follow us :</p>
+          <a href="facebook.fr"><img src="img/Facebook.svg" alt="Facebook" height="50px"></a>
+          <a href="twitter.fr"><img src="img/Twitter.svg" alt="Twitter" height="50px"></a>
+          <a href="googleplus.fr"><img src="img/Google.svg" alt="Google plus" height="50px"></a>
+        </nav>
+
+      </aside>
+      <!-- ################## Aside only for desktop ######################### -->
